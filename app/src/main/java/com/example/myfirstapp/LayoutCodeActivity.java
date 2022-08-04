5031f64143ecd3ce9c180aab40092fe6868d64c5
tmd7619 <tmd814@naver.com>
2022-08-04 21:41:50 +0900

    Update LinerLayOut

--- app/src/main/java/com/example/myfirstapp/LayoutCodeActivity.java
+++ app/src/main/java/com/example/myfirstapp/LayoutCodeActivity.java
@@ -13,17 +13,26 @@ public class LayoutCodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // new 연산자로 리니어 레이아웃을 만들고, 방향 설정
         LinearLayout mainLayout = new LinearLayout(this);
-        mainLayout.setOrientation(LinearLayout.VERTICAL); // new 연산자로 리니어 레이아웃을 만들고, 방향 설정
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
+        // new 연산자로 레이아웃 안에 추가될 뷰들에 설정할 파라미터 생성
+        LinearLayout.LayoutParams params =
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
 
 
+        // 버튼에 파라미터 설정하고 레이아웃에 추가
         Button button11 = new Button(this);
         button11.setText("Button11");
         button11.setLayoutParams(params);
         mainLayout.addView(button11);
+
+        // 새로 만든 레이아웃을 화면에 설정
+        setContentView(mainLayout);
 
 
     }
