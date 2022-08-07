4b7b5f3d65deb0eaf12e3e53ed9471f435add1ae
tmd7619 <tmd814@naver.com>
2022-08-07 13:51:31 +0900

    Add Scroll View

--- app/src/main/java/com/example/myfirstapp/ScrollViewActivity.java
+++ app/src/main/java/com/example/myfirstapp/ScrollViewActivity.java
@@ -1,2 +1,60 @@
-package com.example.myfirstapp;public class ScollViewActivity {
+package com.example.myfirstapp;
+
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrollViewActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    ImageView imageView;
+    BitmapDrawable bitmap;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_scroll);
+
+        // 레이아웃에 정의된 뷰 객체 참조
+        scrollView = findViewById(R.id.scrollView);
+        imageView = findViewById(R.id.imageView);
+
+        scrollView.setHorizontalScrollBarEnabled(true); // 수평 스크롤바 사용 기능 설정
+
+        // 리소스 이미지 참조
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image01);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        // 이미지 리소스와 이미지 크기 설정
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+    }
+
+    public void onButton1Clicked(View v) {
+        changeImage();
+    }
+
+    // 다른 이미지 리소스로 변경
+    private void changeImage() {
+        // 리소스 이미지 참조
+        Resources res = getResources();
+        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image02);
+        int bitmapWidth = bitmap.getIntrinsicWidth();
+        int bitmapHeight = bitmap.getIntrinsicHeight();
+
+        // 이미지 리소스와 이미지 크기 설정
+        imageView.setImageDrawable(bitmap);
+        imageView.getLayoutParams().width = bitmapWidth;
+        imageView.getLayoutParams().height = bitmapHeight;
+
+    }
+
 }
