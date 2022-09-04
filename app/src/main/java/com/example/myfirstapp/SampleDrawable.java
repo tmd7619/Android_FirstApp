40a65b51ba706e7c0b2f81d4f60cb7f03b81769d
tmd7619 <tmd814@naver.com>
2022-09-04 16:35:18 +0900

    drawable

---
+++ app/src/main/java/com/example/myfirstapp/SampleDrawable.java
@@ -0,0 +1,19 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SampleDrawable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+
+        setContentView(R.layout.activity_drawable);
+
+
+    }
+
+
+}
